e122acf0bd521db16a38a7fad61b004c8b4ccd3b
Connor <31197923+ConnorSage@users.noreply.github.com>
2018-07-18 02:11:46 +1200

    non functional login page

--- app/src/main/java/com/example/android/adventurequencher/LoginActivity.java
+++ app/src/main/java/com/example/android/adventurequencher/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.adventurequencher;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +12,11 @@ public class LoginActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final TextInputLayout usernameWrapper = (TextInputLayout) findViewById(R.id.usernameWrapper);
+        final TextInputLayout passwordWrapper = (TextInputLayout) findViewById(R.id.passwordWrapper);
+
+        usernameWrapper.setHint("Username");
+        passwordWrapper.setHint("Password");
     }
 }
