0cb1e82e6943d16aaea820e74efcccb0aff24ead
Connor <31197923+ConnorSage@users.noreply.github.com>
2018-07-28 16:19:31 +1200

    implemented fragment for login screen

--- app/src/main/java/com/example/android/adventurequencher/LoginActivity.java
+++ app/src/main/java/com/example/android/adventurequencher/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.adventurequencher;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,21 +17,10 @@ public class LoginActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // Begin the transaction
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        // Replace the contents of the container with the new fragment
-        ft.add(R.id.login_placeholder, IntroFragment.newInstance());
-        // Complete the changes added above
-        ft.commit();
-
-        /*
-        final TextInputLayout usernameWrapper = (TextInputLayout) findViewById(R.id.usernameWrapper);
-        final TextInputLayout passwordWrapper = (TextInputLayout) findViewById(R.id.passwordWrapper);
-
-        usernameWrapper.setHint("Username");
-        passwordWrapper.setHint("Password");
-
-        login = findViewById(R.id.login);
-        login.setVisibility(LinearLayout.GONE); //hide login input fields*/
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        IntroFragment fragment = new IntroFragment();
+        fragmentTransaction.add(R.id.login_placeholder, fragment);
+        fragmentTransaction.commit();
     }
 }
