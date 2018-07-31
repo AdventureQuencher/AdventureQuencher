ca516639fa86495c616d2171129bdf0b3c357243
AreeMartyn <fvg6708@autuni.ac.nz>
2018-07-31 15:21:58 +1200

    added map styles

--- app/src/main/java/com/example/android/adventurequencher/MenuMaps.java
+++ app/src/main/java/com/example/android/adventurequencher/MenuMaps.java
@@ -1,13 +1,16 @@
 package com.example.android.adventurequencher;
 
+import android.content.res.Resources;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MenuMaps extends FragmentActivity implements OnMapReadyCallback {
@@ -37,7 +40,19 @@ public class MenuMaps extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.mapstyle));
 
+            if (!success) {
+                Log.e("MenuMaps", "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e("MenuMaps", "Can't find style. Error: ", e);
+        }
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
