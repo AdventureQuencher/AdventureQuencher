d5dc401d553f7d4974f576b79b09d1c61e673eda
Darcy Rolleston <fwg7192@autuni.ac.nz>
2018-09-13 21:24:49 +1200

    Changed Google Maps to a Fragment and merged with NavigationView

--- app/src/main/java/com/example/android/adventurequencher/BottomNavigate.java
+++ app/src/main/java/com/example/android/adventurequencher/BottomNavigate.java
@@ -1,33 +1,45 @@
 package com.example.android.adventurequencher;
 
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.TextView;
+
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.List;
 
 public class BottomNavigate extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private FrameLayout mFrame;
+    private MapFragment mapFragment = new MapFragment();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    Intent intent = new Intent(getApplicationContext(),MainQuest.class);
-                    startActivity(intent);
+                   Intent quest = new Intent(getApplicationContext(),MainQuest.class);
+                   startActivity(quest);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                 getSupportFragmentManager().beginTransaction().replace(R.id.m_frame,mapFragment).commit();
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+                   mTextMessage.setText("null");
+                   return true;
             }
             return false;
         }
@@ -39,7 +51,9 @@ public class BottomNavigate extends AppCompatActivity {
         setContentView(R.layout.activity_bottom_navigate);
 
         mTextMessage = findViewById(R.id.message);
+        mFrame = findViewById(R.id.m_frame);
         BottomNavigationView navigation = findViewById(R.id.navigation);
+
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
---
+++ app/src/main/java/com/example/android/adventurequencher/MapFragment.java
@@ -0,0 +1,83 @@
+package com.example.android.adventurequencher;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private MapView mMapView;
+    private View mView;
+
+
+    public MapFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    { super.onCreate(savedInstanceState); }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mView = inflater.inflate(R.layout.fragment_map, container, false);
+        return mView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState)
+    {
+        super.onViewCreated(view,savedInstanceState);
+
+        mMapView = mView.findViewById(R.id.g_map);
+        if(mMapView != null)
+        {
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        MapsInitializer.initialize(getContext());
+
+        mMap = googleMap;
+
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = mMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            getContext(), R.raw.grey_mapstyle));
+
+            if (!success) {
+                Log.e("MapFragment", "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e("MapFragment", "Can't find style. Error: ", e);
+        }
+
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
